d2c88239c5816c33c46dbcc606b243817da40992
saul meneses <fob2257@gmail.com>
2022-05-16 18:33:52 -0700

    User can submit a product

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -7,7 +7,9 @@
           <h1 class="a-size-large a-spacing-none a-text-normal">All products</h1>
           <div class="a-spacing-large" />
 
-          <a href="#" class="a-button-buy-again">Add a new product</a>
+          <a href="/product" class="a-button-buy-again" style="margin-right: 10px">
+            Add a new product
+          </a>
           <a href="#" class="a-button-history margin-right-10">Add a new category</a>
           <a href="#" class="a-button-history margin-right-10">Add a new owner</a>
         </div>
--- admin/pages/product.vue
+++ admin/pages/product.vue
@@ -8,51 +8,91 @@
             <div class="a-spacing-top-medium" />
             <h2 class="a-text-center">Add a new product</h2>
 
+            <!-- <form @submit="handleSubmit"> -->
             <form>
               <div class="a-spacing-top-medium">
                 <label>Category</label>
-                <select class="a-select-option">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
+                <select class="a-select-option" v-model="categoryId">
+                  <option
+                    v-for="category in categories"
+                    :value="category._id"
+                    :key="category._id"
+                  >
+                    {{ category.type }}
+                  </option>
                 </select>
               </div>
 
               <div class="a-spacing-top-medium">
                 <label>Owner</label>
-                <select class="a-select-option">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
+                <select class="a-select-option" v-model="ownerId">
+                  <option v-for="owner in owners" :value="owner._id" :key="owner._id">
+                    {{ owner.name }}
+                  </option>
                 </select>
               </div>
 
               <div class="a-spacing-top-medium">
                 <label style="margin-bottom: 0">Title</label>
-                <input type="text" class="a-input-text" style="width: 100%" />
+                <input
+                  type="text"
+                  class="a-input-text"
+                  style="width: 100%"
+                  v-model="title"
+                />
+              </div>
+
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0">Price</label>
+                <input
+                  type="number"
+                  min="0"
+                  step="1"
+                  class="a-input-text"
+                  style="width: 100%"
+                  v-model="price"
+                />
+              </div>
+
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0">Stock quantity</label>
+                <input
+                  type="number"
+                  min="0"
+                  step="1"
+                  class="a-input-text"
+                  style="width: 100%"
+                  v-model="stockQuantity"
+                />
               </div>
 
               <div class="a-spacing-top-medium">
                 <label style="margin-bottom: 0">Description</label>
-                <textarea placeholder="Product description" style="width: 100%" />
+                <textarea
+                  placeholder="Add a product description"
+                  style="width: 100%"
+                  v-model="description"
+                />
               </div>
 
               <div class="a-spacing-top-medium">
                 <label style="margin-bottom: 0">Photo</label>
                 <div class="a-row a-spacing-top-medium">
-                  <label for="" class="choosefile-button">
-                    <i class="fal fa-plus" />
+                  <label class="choosefile-button">
+                    <i class="fal fa-plus"></i>
+                    <input type="file" @change="handlePhotoInput" />
                   </label>
-                  <input type="file" />
-                  <p>Name of the photo</p>
+                  <p>{{ photoName }}</p>
                 </div>
               </div>
 
               <hr />
               <div class="a-spacing-top-large">
-                <span class="a-button-register">
+                <button type="submit" class="a-button-register" @click="handleSubmit">
                   <span class="a-button-inner">
                     <span class="a-button-text">Add product</span>
                   </span>
-                </span>
+                </button>
               </div>
             </form>
           </div>
@@ -64,5 +104,61 @@
 </template>
 
 <script>
-export default {};
+export default {
+  async asyncData({ $axios }) {
+    try {
+      const [{ categories }, { owners }] = await Promise.all([
+        $axios.$get('http://localhost:3000/api/categories'),
+        $axios.$get('http://localhost:3000/api/owners'),
+      ]);
+
+      return { categories, owners };
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  data() {
+    return {
+      title: '',
+      description: '',
+      price: 1,
+      stockQuantity: 1,
+      categoryId: null,
+      ownerId: null,
+      photo: null,
+      photoName: '',
+    };
+  },
+  methods: {
+    handlePhotoInput(e) {
+      const [file] = e.target.files;
+
+      this.photo = file;
+      this.photoName = file.name;
+    },
+    async handleSubmit(e) {
+      e.preventDefault();
+
+      try {
+        const { title, description, price, stockQuantity, categoryId, ownerId, photo } =
+          this;
+
+        const data = new FormData();
+        data.append('title', title);
+        data.append('description', description);
+        data.append('price', price);
+        data.append('stockQuantity', stockQuantity);
+        data.append('categoryId', categoryId);
+        data.append('ownerId', ownerId);
+        data.append('photo', photo, photo.name);
+
+        await this.$axios.$post('http://localhost:3000/api/products', data);
+
+        this.$router.push('/');
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
 </script>
