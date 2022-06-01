29dac88bff845f68a687661e4ade5e3e2b2053d2
saul meneses <fob2257@gmail.com>
2022-05-31 17:00:13 -0700

    Verify user token

---
+++ server/middlewares/verifyToken.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const bearer = "Bearer ";
+  let token = req.headers["x-access-token"] || req.headers["authorization"];
+  const isBearer = token.startsWith(bearer);
+
+  if (!token || !isBearer) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Bearer token is required" });
+  }
+
+  token = token.slice(bearer.length, token.length);
+
+  jwt.verify(token, process.env.JWT_PRIVATE_KEY, (error, decoded) => {
+    if (error) {
+      return res.status(400).json({ success: false, message: error });
+    }
+
+    req.decoded = decoded;
+
+    next();
+  });
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const verifyToken = require("../middlewares/verifyToken");
 
 router.post("/auth/signup", async (req, res) => {
   const { name, email, password } = req.body;
@@ -30,6 +31,26 @@ router.post("/auth/signup", async (req, res) => {
     );
 
     res.json({ success: true, token });
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
+
+router.get("/auth", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.decoded.email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+
+    user.password = undefined;
+
+    res.json({ success: true, user });
   } catch (err) {
     console.error(err);
 
